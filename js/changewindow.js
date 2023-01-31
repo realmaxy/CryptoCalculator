bcc401a0a5763c55c4d23b75a95763127d58b129
realmaxy <max_sem777@mail.ru>
2023-01-31 10:31:13 +0300

    adapted changewindow.js to new design

--- js/changewindow.js
+++ js/changewindow.js
@@ -1,14 +1,16 @@
 const conveterWindow = document.querySelector(".converter")
-const converterBtn = document.querySelector(".converter__btn")
+const converterBtn = document.querySelector(".converter_btn")
 
 const profitcalcWindow = document.querySelector(".profitcalc")
-const profitcalcBtn = document.querySelector(".profitcalc__btn")
+const profitcalcBtn = document.querySelector(".profitcalc_btn")
 
 function swipeWindow(event) {
     if(event.target == converterBtn) {
         profitcalcWindow.classList.add("none")
+        conveterWindow.classList.remove("none")
     }
     if(event.target == profitcalcBtn) {
+        conveterWindow.classList.add("none")
         profitcalcWindow.classList.remove("none")
     }
 }
