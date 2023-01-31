6a6b40eabebe186d1636cd55a75ec895c246375b
realmaxy <max_sem777@mail.ru>
2023-01-31 13:13:22 +0300

    adapted calcprofit.js to new design

--- js/calcprofit.js
+++ js/calcprofit.js
@@ -1,2 +1,34 @@
 const selectCurBtn = document.querySelector(".profitcalc__dropdown_button")
-const 
+const purchasePriceInput = document.querySelector(".profitcalc__input_purchasePrice")
+const sellingPriceInput = document.querySelector(".profitcalc__input_sellingPrice")
+const investmentsInput = document.querySelector(".profitcalc__input_investmentsInput")
+const infoProfit = document.querySelector(".profitcalc__info_profit")
+const infoTotalAmount = document.querySelector(".profitcalc__info_totalamount")
+const profitcalcDropItems = document.querySelectorAll(".profitcalc_dropItem")
+
+//Ввод цены выбранной валюты в инпуты
+for(let elem of profitcalcDropItems) {
+    elem.addEventListener("click", event => {
+        const currency = event.target.textContent
+        purchasePriceInput.value = (Currencies[currency].values.USD.price).toFixed(2)
+        sellingPriceInput.value = (Currencies[currency].values.USD.price).toFixed(2)
+    })
+}
+
+// Вывод профита и общей суммы при выходе
+
+investmentsInput.addEventListener("input", renderProfit)
+
+function renderProfit() {
+    const buyValue = Number(purchasePriceInput.value)
+    const sellValue = Number(sellingPriceInput.value)
+    const invValue = Number(investmentsInput.value)
+
+    const totalAmount = invValue / buyValue * sellValue
+    const profit = totalAmount - invValue
+
+    if(totalAmount.textContent != '' && sellValue.textContent != '') {
+        infoProfit.textContent = `$${profit.toFixed(3)}`
+        infoTotalAmount.textContent = `$${totalAmount.toFixed(3)}`
+    }
+}
