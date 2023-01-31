5b89c1b74dafd400d6096cf7a8630aa2b24eea06
realmaxy <max_sem777@mail.ru>
2023-01-31 11:30:31 +0300

    finished dising

--- js/calcprofit.js
+++ js/calcprofit.js
@@ -1,14 +1,2 @@
-const profitCalcInputCount = document.querySelector(".input__count")
-const profitCalcInputPrice = document.querySelector(".price__count")
-const profitCalcChooseCurrencie = document.querySelector(".profitcalcDropBtn")
-const calcButton = document.querySelector(".calcbutton")
-
-calcButton.addEventListener("click", ()=> {
-    if(!(profitCalcChooseCurrencie.textContent == "Выберите валюту")) {
-        let cost = Currencies[profitCalcChooseCurrencie.textContent].values.USD.price
-        let profit = (cost - profitCalcInputPrice.value) * profitCalcInputCount.value
-
-    }
-})
-
-
+const selectCurBtn = document.querySelector(".profitcalc__dropdown_button")
+const 
