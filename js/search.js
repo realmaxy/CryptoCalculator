c52229cfe4b9b7854a075818fc76bbcc8ca17315
realmaxy <max_sem777@mail.ru>
2023-03-04 13:39:50 +0300

    add search to all dropdowns

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-searchInp = document.querySelector('.search_inp')
+searchInp = document.querySelectorAll('.search_inp')
 drList = document.querySelector('.profitcalc_dr_list')
 
 const renderDropdown = (dropdown, list) => {
@@ -16,9 +16,9 @@ const renderDropdown = (dropdown, list) => {
         dropdown.append(newDiv)
     }
 
+    
+
     const dropItems = document.querySelectorAll(".dropdown__content_item")
-    const purchasePriceInput = document.querySelector(".profitcalc__input_purchasePrice")
-    const sellingPriceInput = document.querySelector(".profitcalc__input_sellingPrice")
 
     for(let item of dropItems) {
         item.addEventListener("click", () =>{
@@ -26,28 +26,35 @@ const renderDropdown = (dropdown, list) => {
             item.parentElement.parentElement.previousElementSibling.textContent = currencieName
             item.parentElement.parentElement.classList.toggle("none")
             
-            for(key in Currencies) {
-                if(Currencies[key].symbol === currencieName) {
-                    purchasePriceInput.value = Currencies[key].price.toFixed(2)
-                    sellingPriceInput.value = Currencies[key].price.toFixed(2)
+            if (dropdown === drList) {
+                const purchasePriceInput = document.querySelector(".profitcalc__input_purchasePrice")
+                const sellingPriceInput = document.querySelector(".profitcalc__input_sellingPrice")
+                
+                for(key in Currencies) {
+                    if(Currencies[key].symbol === currencieName) {
+                        purchasePriceInput.value = Currencies[key].price.toFixed(2)
+                        sellingPriceInput.value = Currencies[key].price.toFixed(2)
+                    }
                 }
             }
         })
     }
-
 }
 
-searchInp.addEventListener('input', (event)=>{
-    const curList = {...Currencies}
-    wordSize = event.target.value.length
-    let newList = []
 
-    for(key in curList) {
-        if(curList[key].symbol.slice(0, wordSize) == event.target.value) {
-            let newItem = curList[key].symbol
-            newList.push(newItem)
+for(let item of searchInp) {
+    item.addEventListener('input', (event)=>{
+        const curList = {...Currencies}
+        wordSize = event.target.value.length
+        let newList = []
+        const dropdown = event.target.parentElement.nextElementSibling
+
+        for(key in curList) {
+            if(curList[key].symbol.slice(0, wordSize) == event.target.value) {
+                let newItem = curList[key].symbol
+                newList.push(newItem)
+            }
         }
-    }
-    renderDropdown(drList, newList)
-})
-
+        renderDropdown(dropdown, newList)
+    })
+}
